79366c27677e32f6f836b7cc11f54ae29901d076
Marcelo <mvrozanti@hotmail.com>
2021-11-18 22:43:31 -0300

    added second initialize function

--- src/main/java/org/octree/Octree.java
+++ src/main/java/org/octree/Octree.java
@@ -43,7 +43,6 @@ public class Octree<T extends Number> {
         else                                /*                                                      **/
             data = points;                  /*                                                      **/
 
-
         final int N = points.size();
         successors = new ArrayList<>(N);
 
@@ -91,7 +90,65 @@ public class Octree<T extends Number> {
     }
 
     public void initialize(List<OctPoint<T>> points, List<Integer> indexes, OctreeParams octreeParams) {
+        clear();
+        params = octreeParams;
 
+        if (params.getCopyPoints())         /** is this an accurate translation for that condition? **/
+            data = new ArrayList<>(points); /*                                                      **/
+        else                                /*                                                      **/
+            data = points;                  /*                                                      **/
+
+        final int N = points.size();
+        successors = new ArrayList<>(N);
+
+        if (indexes.isEmpty())
+            return;
+
+        int lastIdx = indexes.get(0);
+        List<T> min = new ArrayList<>(3);
+        List<T> max = new ArrayList<>(3);
+        min.set(0, points.get(lastIdx).x());
+        min.set(1, points.get(lastIdx).y());
+        min.set(2, points.get(lastIdx).z());
+        max.set(0, min.get(0));
+        max.set(1, min.get(1));
+        max.set(2, min.get(2));
+
+        for(int i = 1; i < indexes.size(); ++i) {
+            int idx = indexes.get(i);
+            successors.set(lastIdx, idx);
+            OctPoint<T> p = points.get(idx);
+            if (p.x().floatValue() < min.get(0).floatValue())
+                min.set(0, p.x());
+            if (p.y().floatValue() < min.get(1).floatValue())
+                min.set(1, p.y());
+            if (p.z().floatValue() < min.get(2).floatValue())
+                min.set(2, p.z());
+            if (p.x().floatValue() > max.get(0).floatValue())
+                max.set(0, p.x());
+            if (p.y().floatValue() > max.get(1).floatValue())
+                max.set(1, p.y());
+            if (p.z().floatValue() > max.get(2).floatValue())
+                max.set(2, p.z());
+
+            lastIdx = idx;
+        }
+
+        List<Float> ctr = new ArrayList<>(3);
+        ctr.add(min.get(0).floatValue());
+        ctr.add(min.get(1).floatValue());
+        ctr.add(min.get(2).floatValue());
+
+        float maxextent = 0.5f * (max.get(0).floatValue() - min.get(0).floatValue());
+        ctr.set(0, ctr.get(0) + maxextent);
+        for (int i = 1; i < 3; ++i) {
+            float extent = 0.5f * (max.get(i).floatValue() - min.get(i).floatValue());
+            ctr.set(i, ctr.get(i) + extent);
+            if (extent > maxextent)
+                maxextent = extent;
+        }
+
+        root = createOctant(ctr.get(0), ctr.get(1), ctr.get(2), maxextent, indexes.get(0), lastIdx, indexes.size());
     }
 
     public void clear() {
