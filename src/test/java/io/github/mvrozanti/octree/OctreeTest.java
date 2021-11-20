a8db4d5aa42e7fde61ab082ce9d00a16ab7bc71f
Marcelo <mvrozanti@hotmail.com>
2021-11-20 14:24:24 -0300

    add initialization test with min extent

--- src/test/java/io/github/mvrozanti/octree/OctreeTest.java
+++ src/test/java/io/github/mvrozanti/octree/OctreeTest.java
@@ -37,10 +37,118 @@ public class OctreeTest {
     }
 
     @Test
+    void testInitializationWithMinExtent() {
+        int N = 1000;
+        OctreeParams params = new OctreeParams();
+        params.setBucketSize(16);
+
+        Octree oct = new Octree();
+
+        Octant root = oct.root;
+
+        assertNull(root);
+
+        List<PointT> points = new ArrayList<>(N);
+        randomPoints(points, N, 1337);
+
+        oct.initialize(points, params);
+
+        root = oct.root;
+        assertNotNull(root);
+        assertEquals(N, oct.successors.size());
+
+        List<Integer> elementCount = new ArrayList<>(Collections.nCopies(N, 0));
+        int idx = root.getStart();
+        for (int i = 0; i < N; i++) {
+            assertTrue(idx < N);
+            assertTrue(oct.successors.get(idx) <= N);
+            int count = elementCount.get(idx);
+            elementCount.set(idx, count + 1);
+            assertEquals(1, elementCount.get(idx));
+            idx = oct.successors.get(idx);
+        }
+
+        for (int i = 0; i < N; i++)
+            assertEquals(1, elementCount.get(i));
+
+        ArrayDeque<Octant> queue = new ArrayDeque<>();
+        queue.push(root);
+        List<Integer> assignment = new ArrayList<>(Collections.nCopies(N, -1));
+
+        while (!queue.isEmpty()) {
+            Octant octant = queue.pop();
+
+            assertTrue(octant.getStart() < N);
+
+            int octantIdx = octant.getStart();
+            int lastIdx = octant.getStart();
+
+            for (int i = 0; i < octant.getSize(); i++) {
+                double x = points.get(octantIdx).x() - octant.getX();
+                double y = points.get(octantIdx).y() - octant.getY();
+                double z = points.get(octantIdx).z() - octant.getZ();
+
+                assertTrue(abs(x) <= octant.getExtent());
+                assertTrue(abs(y) <= octant.getExtent());
+                assertTrue(abs(z) <= octant.getExtent());
+
+                assignment.set(octantIdx, -1);
+                lastIdx = octantIdx;
+                octantIdx = oct.successors.get(octantIdx);
+            }
+            assertEquals(octant.getEnd(), lastIdx);
+
+            boolean shouldBeLeaf = true;
+            Octant firstchild = null;
+            Octant lastchild = null;
+            int pointSum = 0;
+
+            for (int c = 0; c < 8; ++c) {
+                Octant child = octant.getChild(c);
+                if (child == null)
+                    continue;
+                shouldBeLeaf = false;
+                if (firstchild == null)
+                    firstchild = child;
+                if (lastchild != null)
+                    assertEquals(child.getStart(), oct.successors.get(lastchild.getEnd()));
+
+                pointSum += child.getSize();
+                lastchild = child;
+                int childIdx = child.getStart();
+                for (int i = 0; i < child.getSize(); i++) {
+                    assertEquals(-1, assignment.get(childIdx));
+                    assignment.set(childIdx, c);
+                    childIdx = oct.successors.get(childIdx);
+                }
+                queue.push(child);
+            }
+
+            if (firstchild != null)
+                assertEquals(octant.getStart(), firstchild.getStart());
+            if (lastchild != null)
+                assertEquals(octant.getEnd(), lastchild.getEnd());
+
+            assertEquals(shouldBeLeaf, octant.isLeaf());
+            assertEquals(octant.getSize() <= params.getBucketSize() || octant.getExtent() < 2 * params.getMinExtent(), octant.isLeaf());
+
+            if (!octant.isLeaf()) {
+                assertEquals(octant.getSize(), pointSum);
+                int leafIdx = octant.getStart();
+                for (int i = 0; i < octant.getSize(); i++) {
+                    assertTrue(assignment.get(leafIdx) > -1);
+                    leafIdx = oct.successors.get(leafIdx);
+                }
+            }
+        }
+    }
+
+    @Test
     void testInitialization() {
         int N = 1000;
         OctreeParams params = new OctreeParams();
         params.setBucketSize(16);
+        params.setMinExtent(1);
 
         Octree oct = new Octree();
 
